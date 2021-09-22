0653a3324815040f04befedabb3344fac336323d
Adrien-M-design <adrien.marchello1@gmail.com>
2021-09-22 16:01:53 +0200

    wéwé

--- Out of Range/Assets/Character/CharacterController.cs
+++ Out of Range/Assets/Character/CharacterController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D _rb = null;
+    [SerializeField] private float _torqueForce = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,8 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float turn = InputManager.Instance.Rot.x;
+        _rb.AddTorque(_torqueForce * turn);
     }
 
     public void Jump()
